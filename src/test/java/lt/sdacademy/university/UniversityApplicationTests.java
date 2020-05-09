d18da08cdd90f5b57b67740006506c478b7795e1
Rutenis Turcinas <rutenis.turcinas@devbridge.com>
2020-05-09 11:45:35 +0300

    Initial Spring Boot project

---
+++ src/test/java/lt/sdacademy/university/UniversityApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.sdacademy.university;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
