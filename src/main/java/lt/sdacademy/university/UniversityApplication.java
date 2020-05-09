dd3efdb1313cfb59e3ff3f5053d10bad11edfb25
Rutenis Turcinas <rutenis.turcinas@devbridge.com>
2020-05-09 11:48:12 +0300

    Println

--- src/main/java/lt/sdacademy/university/UniversityApplication.java
+++ src/main/java/lt/sdacademy/university/UniversityApplication.java
@@ -8,5 +8,7 @@ public class UniversityApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UniversityApplication.class, args);
+
+        System.out.println("SDA");
     }
 }
