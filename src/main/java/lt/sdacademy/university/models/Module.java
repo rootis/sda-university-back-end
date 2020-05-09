4336575f9a3dc91430dfa4ebe9c1be7b5fc40934
Rutenis Turcinas <rutenis.turcinas@devbridge.com>
2020-05-09 13:19:53 +0300

    Module

---
+++ src/main/java/lt/sdacademy/university/models/Module.java
@@ -0,0 +1,29 @@
+package lt.sdacademy.university.models;
+
+public class Module {
+
+    private Integer id;
+
+    private String title;
+
+    public Module(Integer id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
