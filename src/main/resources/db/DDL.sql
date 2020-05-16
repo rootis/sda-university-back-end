5f46df7d2c3f9db4ec76a7bd5aad69009c8e37bf
Rutenis Turcinas <rutenis.turcinas@devbridge.com>
2020-05-16 13:14:12 +0300

    Study program

--- src/main/resources/db/DDL.sql
+++ src/main/resources/db/DDL.sql
@@ -5,3 +5,11 @@ CREATE TABLE university
     code varchar(10) not null,
     title varchar(250) not null
 );
+
+CREATE TABLE study_program
+(
+    id int unsigned auto_increment primary key,
+    title varchar(250) not null,
+    university_id int unsigned not null,
+    foreign key (university_id) references university(id)
+);
--- src/main/resources/db/DML.sql
+++ src/main/resources/db/DML.sql
@@ -1,3 +1,7 @@
 -- DML
 insert into university (code, title) values ('KTU', 'Kaunas University of Technology');
 insert into university (code, title) values ('VU', 'Vilnius University');
+
+insert into study_program (title, university_id) VALUES ('Multimedia Technologies', (select id from university where code = 'KTU'));
+insert into study_program (title, university_id) VALUES ('Informatics', (select id from university where code = 'KTU'));
+insert into study_program (title, university_id) VALUES ('Software Systems', (select id from university where code = 'KTU'));
