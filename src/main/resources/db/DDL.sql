edcb3fe7a03988501af09387bab592a82ac0d0fc
Rutenis Turcinas <rutenis.turcinas@devbridge.com>
2020-05-17 11:02:10 +0300

    DDL, DML

--- src/main/resources/db/DDL.sql
+++ src/main/resources/db/DDL.sql
@@ -1,15 +1,30 @@
 -- DDL
 CREATE TABLE university
 (
-    id int unsigned auto_increment primary key,
-    code varchar(10) not null,
+    id    int unsigned auto_increment primary key,
+    code  varchar(10)  not null,
     title varchar(250) not null
 );
 
 CREATE TABLE study_program
 (
-    id int unsigned auto_increment primary key,
-    title varchar(250) not null,
+    id            int unsigned auto_increment primary key,
+    title         varchar(250) not null,
     university_id int unsigned not null,
-    foreign key (university_id) references university(id)
+    foreign key (university_id) references university (id)
+);
+
+CREATE TABLE module
+(
+    id    int unsigned auto_increment primary key,
+    title varchar(250) not null
+);
+
+CREATE TABLE study_program_module
+(
+    study_program_id int unsigned not null,
+    module_id        int unsigned not null,
+    foreign key (study_program_id) references study_program (id),
+    foreign key (module_id) references module (id),
+    unique (study_program_id, module_id)
 );
