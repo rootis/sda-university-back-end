4891f85df108b0b3f540194b8e8e6ec03306ae95
Rutenis Turcinas <rutenis.turcinas@devbridge.com>
2020-05-17 09:38:29 +0300

    Thymeleaf

--- src/main/java/lt/sdacademy/university/controllers/UniversityController.java
+++ src/main/java/lt/sdacademy/university/controllers/UniversityController.java
@@ -1,22 +1,11 @@
 package lt.sdacademy.university.controllers;
 
-import java.util.List;
-import lt.sdacademy.university.models.dto.University;
 import lt.sdacademy.university.services.UniversityService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin
-@RestController
-@RequestMapping("/api/universities")
+@Controller
 public class UniversityController {
 
     private final UniversityService universityService;
@@ -26,24 +15,11 @@ public class UniversityController {
     }
 
     @GetMapping
-    public List<University> getAll() {
-        return universityService.getUniversities();
-    }
+    public String getUniversities(Model model) {
+        model.addAttribute("headerText", "SDA Lecture");
+        model.addAttribute("title", "Universities");
+        model.addAttribute("universities", universityService.getUniversities());
 
-    @GetMapping("/{id}")
-    public University getUniversity(@PathVariable Integer id) {
-        return universityService.getUniversity(id);
-    }
-
-    @PostMapping
-    public University save(@RequestBody University university) {
-        return universityService.save(university);
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Integer id) {
-        universityService.delete(id);
-
-        return ResponseEntity.status(HttpStatus.OK).build();
+        return "universities";
     }
 }
